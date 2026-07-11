61b1895ad5e1f44ffdec208027fed6fe01cb6c90
Maxym <hako.diaha@gmail.com>
2026-07-11 21:21:37 +0200

    cin/out

--- main.cpp
+++ main.cpp
@@ -2,8 +2,28 @@
 
 int main(void) {
 
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "My new beginning there." << std::endl;
+    char storage[] = "Establish";
+    unsigned int number = 3000000000;
+    float point_num = 3.1415962535;
+
+    std::cout << "Before: " << '\n' << storage << '\n' << number << '\n' << point_num << '\n';
+    std::cout << "----------------" << '\n';
+
+    std::cout << "Enter new variables: " << '\n';
+
+    std::cin >> storage; // it reads only to the first space
+    std::cin >> number;
+    std::cin >> point_num;
+
+    std::cout << "----------------" << '\n';
+
+    std::cout << "After: " << '\n';
+
+    std::cout << storage << '\n';
+    std::cout << number << '\n';
+    std::cout << point_num << '\n';
+
+    std::cout << "----------------" << '\n';
 
     return 0;
 }
