6020c3cafd331e99821df18b0e5f6a46ccea7a1f
Maxym <hako.diaha@gmail.com>
2026-07-15 21:55:26 +0200

    added modul functions to calculator

--- calculatorProject/calculator.cpp
+++ calculatorProject/calculator.cpp
@@ -1,47 +1,85 @@
 #include <iostream>
-#include "calculatorFunctions.h"
+#include <limits>
+//#include "calculatorFunctions.h"
+
+double get_number()
+{
+    double number = 0;
+    while (!(std::cin >> number))
+    {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+
+    return number;
+}
+
+
+char get_operation_sign()
+{
+    char operation;
+    std::cin >> operation;
+    
+    return operation;
+}
+
+
+int check_operation_sign(char operation)
+{
+    if(operation == '+' || operation == '-' || 
+       operation == '*' || operation == '/')
+        return true;
+    else
+        return false;
+}
+
+
+void print_zero_division_err()
+{
+    std::cout << "0 zero division error" << '\n';
+}
+
+
+void print_result(double a, double operation, char b)
+{
+    if(operation == '+')
+        std::cout << a + b << '\n';
+
+    else if(operation == '-')
+        std::cout << a - b << '\n';
+
+    else if(operation == '*')
+        std::cout << a * b << '\n';
+
+    else if(operation == '/')
+        std::cout << a / b << '\n';        
+    
+    else 
+        return;
+
+}
 
 int main() {
 
-    double a = 0;
-    char operation;
-    double b = 0;
-    double result = 0;
+    double a = get_number();
+    char operation = get_operation_sign();
+    double b = get_number();
 
-    std::cin >> a;
-    std::cin >> operation;
-    std::cin >> b;
+
+    if(!(check_operation_sign(operation))) {
+        std::cout << "Error sign" << '\n';
+        return 1;
+    }
+
 
     if(operation == '/' && b == 0.0) {
-        std::cout << "0 zero division error" << '\n';
+        print_zero_division_err();
         return 1;
     }
-
-
-    switch (operation)
-    {
-    case '+':
-        result = add(a, b);
-        break;
     
-    case '-':
-        result = subtract(a, b);
-        break;
-    
-    case '*':
-        result = multiply(a, b);
-        break;
+    std::cout << "main works" << '\n';
 
-    case '/':
-        result = divide(a, b);
-        break;
-
-    default:
-        show_err();
-        return 1;
-    }
-
-    std::cout << result << '\n';
+    print_result(a, operation, b);
     
     return 0;
 }
